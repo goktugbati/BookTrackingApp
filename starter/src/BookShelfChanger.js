bf85e1ac3eb479fb437fd45e2895bebb29945ea4
goktug <goktug.bati@gmail.com>
2022-12-27 01:22:25 +0300

    type: refactor
    Made shelves an array so that options in select tag can be generated by using map functionality of javascript

--- starter/src/BookShelfChanger.js
+++ starter/src/BookShelfChanger.js
@@ -2,25 +2,47 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 const BookShelfChanger = ({handleChangeStatus, shelf}) => {
+    const shelves = [
+        {
+            id: 1,
+            shelfName: 'currentReading',
+            shelfDisplayName: 'Currently Reading'
+        },
+        {
+            id: 2,
+            shelfName: 'wantToRead',
+            shelfDisplayName: 'Want to Read'
+        },
+        {
+            id: 3,
+            shelfName: 'read',
+            shelfDisplayName: 'Read'
+        },
+        {
+            id: 4,
+            shelfName: 'none',
+            shelfDisplayName: 'None'
+        },
+    ]
 
     const handleChangeOption = (e) => {
         handleChangeStatus(e.target.value);
     }
- return(
-     <div className="book-shelf-changer">
-         <select onChange={handleChangeOption} value={shelf ? shelf : 'none'}>
-             <option disabled>
-                 Move to...
-             </option>
-             <option value="currentlyReading">
-                 Currently Reading
-             </option>
-             <option value="wantToRead">Want to Read</option>
-             <option value="read">Read</option>
-             <option value="none">None</option>
-         </select>
-     </div>
- )
+    return (
+        <div className="book-shelf-changer">
+            <select onChange={handleChangeOption} value={shelf ? shelf : 'none'}>
+                <option disabled>
+                    Move to...
+                </option>
+                {shelves.map(
+                    shelf =>
+                        <option key={shelf.id} value={shelf.name}>
+                            {shelf.shelfDisplayName}
+                        </option>)
+                }
+            </select>
+        </div>
+    )
 }
 
 export default BookShelfChanger;
